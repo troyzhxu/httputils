d9d65e9299629560de4ad0baaba1e9bda048372a
周旭 <zhou.xu@ejlchina.com>
2020-03-16 17:31:17 +0800

    注释

--- src/main/java/com/ejlchina/http/Preprocessor.java
+++ src/main/java/com/ejlchina/http/Preprocessor.java
@@ -6,16 +6,28 @@ package com.ejlchina.http;
  */
 public interface Preprocessor {
 
-	
+	/**
+	 * 在HTTP请求开始之前执行
+	 * @param process HTTP请求进程
+	 */
 	void beforeReqest(Process process);
 	
 	
 	interface Process {
 		
+		/**
+		 * @return 当前的请求任务
+		 */
 		HttpTask<? extends HttpTask<?>> getHttpTask();
 		
+		/**
+		 * @return HttpClient
+		 */
 		HttpClient getHttpClient();
 		
+		/**
+		 * 继续HTTP请求任务
+		 */
 		void proceed();
 		
 	}
