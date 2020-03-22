d0060b2188a1b5c7154a15a4da8c7a9a4ac1b391
周旭 <15735559897@qq.com>
2020-03-22 17:03:54 +0800

    。

--- src/main/java/com/ejlchina/http/internal/ResultBody.java
+++ src/main/java/com/ejlchina/http/internal/ResultBody.java
@@ -91,7 +91,7 @@ public class ResultBody implements Body {
 			if (stepBytes <= 0) {
 				stepBytes = DEFAULT_STEP_BYTES;
 			}
-			return new ProcessStream(input, onProcess, totalBytes, stepBytes, 
+			return new ProcessInputStream(input, onProcess, totalBytes, stepBytes, 
 					rangeIgnored ? 0 : rangeStart, callbackExecutor);
 		}
 		return input;
